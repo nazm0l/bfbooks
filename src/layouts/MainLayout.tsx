d0b0ff056d13372a63bb379c8eef82348eaded9d
nazm0l <tanjirahmednazmul@gmail.com>
2023-07-31 00:43:05 +0600

    footer fixed

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function MainLayout() {
   return (
@@ -8,6 +9,7 @@ export default function MainLayout() {
       <div className="pt-16 px-32">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 }
